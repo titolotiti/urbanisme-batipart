7dd4b7001f99277b20fbdc1823d7a8b8a849647c
titolotiti <thibaudgirard989@gmail.com>
2026-06-15 17:40:11 +0200

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-import { createRequire } from 'module';
+ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const pdfParse = require('pdf-parse/lib/pdf-parse.js');
 
